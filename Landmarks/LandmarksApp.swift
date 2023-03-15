70d7bb5636da93e7c157dae1d22430de45158962
x43125 <wx43125@163.com>
2023-03-15 23:47:32 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 王详 on 2023/3/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
